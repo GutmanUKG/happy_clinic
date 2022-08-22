4e04039f352238390bc8c823fae9489492110d70
Vladimir <vgutman95@mail.ru>
2022-08-22 17:12:30 +0600

    'add_page'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-
+    const toggleForm = document.querySelector('#toggle_form')
+    const formChild = document.querySelector('.form_child')
+    const formAdult = document.querySelector('.form_adult')
+    toggleForm.addEventListener('click', ()=>{
+        toggleForm.classList.toggle('adult')
+        if(toggleForm.classList.contains('adult')){
+            formAdult.style.display = 'block'
+            formChild.style.display = 'none'
+        }else{
+            formAdult.style.display = 'none'
+            formChild.style.display = 'block'
+        }
+    })
 })
--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 "use strict";
 
-document.addEventListener('DOMContentLoaded', function () {});
+document.addEventListener('DOMContentLoaded', function () {
+  var toggleForm = document.querySelector('#toggle_form');
+  var formChild = document.querySelector('.form_child');
+  var formAdult = document.querySelector('.form_adult');
+  toggleForm.addEventListener('click', function () {
+    toggleForm.classList.toggle('adult');
+
+    if (toggleForm.classList.contains('adult')) {
+      formAdult.style.display = 'block';
+      formChild.style.display = 'none';
+    } else {
+      formAdult.style.display = 'none';
+      formChild.style.display = 'block';
+    }
+  });
+});
 //# sourceMappingURL=main.js.map
