88c217b77584bb0ed6421dd697fce76660c4913c
Vladimir <vgutman95@mail.ru>
2022-08-23 11:57:09 +0600

    'add_popup'

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,4 +12,73 @@ document.addEventListener('DOMContentLoaded', ()=>{
             formChild.style.display = 'block'
         }
     })
+
+    function closeElement(btnClass, elementClass){
+        const element = document.querySelector(elementClass)
+        let btn = element.querySelector(btnClass)
+
+        btn.addEventListener('click', (e)=>{
+            e.preventDefault()
+            element.classList.remove('active')
+            document.body.style.overflow = ''
+        })
+    }
+    closeElement('.btn_close', '.popup')
+
+
+    //stepsForm
+    try{
+        const popup = document.querySelector('.popup')
+        const popupMess = popup.querySelector('.popup_mess')
+        const resultMess = popup.querySelector('.result_mess')
+        const btnNextStep = popup.querySelector('.form_submit')
+        let step = 0;
+        const btnToggleBoll = popup.querySelector('.btn_toggle_boll')
+        //Переменная для проверки вывода всплываши с ошибкой
+        let isError = false
+        btnNextStep.addEventListener('click', (e)=>{
+            e.preventDefault()
+            step++
+            if(step > 0){
+                popupMess.style.display = 'none'
+                resultMess.style.display = 'block'
+                if(isError === true){
+                    popup.classList.add('error')
+                    popup.style.cssText = `
+                        width: 736px;
+                        height: 334px;
+                        background: #FFF url(../assets/imgs/optimize_imgs/popup_bg_error.png) right/contain no-repeat;
+                    `
+                }else{
+                    popup.classList.remove('error')
+                    popup.style.cssText = `
+                        width: 736px;
+                        height: 334px;
+                        background: #FFF url(../assets/imgs/optimize_imgs/popup_bg_check.png) right/contain no-repeat;
+                    `
+                }
+            }
+        })
+
+        btnToggleBoll.addEventListener('click', ()=>{
+            isError = !isError
+            if(isError === true){
+                popup.classList.add('error')
+                popup.style.cssText = `
+                        width: 736px;
+                        height: 334px;
+                        background: #FFF url(../assets/imgs/optimize_imgs/popup_bg_error.png) right/contain no-repeat;
+                    `
+            }else{
+                popup.classList.remove('error')
+                popup.style.cssText = `
+                        width: 736px;
+                        height: 334px;
+                        background: #FFF url(../assets/imgs/optimize_imgs/popup_bg_check.png) right/contain no-repeat;
+                    `
+            }
+        })
+    }catch(e){
+        console.error(e)
+    }
 })
--- js/main.js
+++ js/main.js
@@ -15,5 +15,58 @@ document.addEventListener('DOMContentLoaded', function () {
       formChild.style.display = 'block';
     }
   });
+
+  function closeElement(btnClass, elementClass) {
+    var element = document.querySelector(elementClass);
+    var btn = element.querySelector(btnClass);
+    btn.addEventListener('click', function (e) {
+      e.preventDefault();
+      element.classList.remove('active');
+      document.body.style.overflow = '';
+    });
+  }
+
+  closeElement('.btn_close', '.popup'); //stepsForm
+
+  try {
+    var popup = document.querySelector('.popup');
+    var popupMess = popup.querySelector('.popup_mess');
+    var resultMess = popup.querySelector('.result_mess');
+    var btnNextStep = popup.querySelector('.form_submit');
+    var step = 0;
+    var btnToggleBoll = popup.querySelector('.btn_toggle_boll'); //Переменная для проверки вывода всплываши с ошибкой
+
+    var isError = false;
+    btnNextStep.addEventListener('click', function (e) {
+      e.preventDefault();
+      step++;
+
+      if (step > 0) {
+        popupMess.style.display = 'none';
+        resultMess.style.display = 'block';
+
+        if (isError === true) {
+          popup.classList.add('error');
+          popup.style.cssText = "\n                        width: 736px;\n                        height: 334px;\n                        background: #FFF url(../assets/imgs/optimize_imgs/popup_bg_error.png) right/contain no-repeat;\n                    ";
+        } else {
+          popup.classList.remove('error');
+          popup.style.cssText = "\n                        width: 736px;\n                        height: 334px;\n                        background: #FFF url(../assets/imgs/optimize_imgs/popup_bg_check.png) right/contain no-repeat;\n                    ";
+        }
+      }
+    });
+    btnToggleBoll.addEventListener('click', function () {
+      isError = !isError;
+
+      if (isError === true) {
+        popup.classList.add('error');
+        popup.style.cssText = "\n                        width: 736px;\n                        height: 334px;\n                        background: #FFF url(../assets/imgs/optimize_imgs/popup_bg_error.png) right/contain no-repeat;\n                    ";
+      } else {
+        popup.classList.remove('error');
+        popup.style.cssText = "\n                        width: 736px;\n                        height: 334px;\n                        background: #FFF url(../assets/imgs/optimize_imgs/popup_bg_check.png) right/contain no-repeat;\n                    ";
+      }
+    });
+  } catch (e) {
+    console.error(e);
+  }
 });
 //# sourceMappingURL=main.js.map
